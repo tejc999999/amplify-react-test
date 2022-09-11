31e52c2512c53c8427a17917db54e37973192328
user01 <user01@user01noMacBook-Air.local>
2022-09-12 02:50:16 +0900

    test router6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+// https://reffect.co.jp/react/react-router-6
 /* src/App.js */
-
+import { Routes, Route, Link } from 'react-router-dom';
 import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+
+import Todo from './Todo';
 
 /* src/App.js */
 function App({ signOut, user }) {
@@ -12,6 +15,11 @@ function App({ signOut, user }) {
       {/* Add Todo JSX here  */}
       <Heading level={1}>Hello {user.username}</Heading>
       <Button onClick={signOut}>Sign out</Button>
+      <Routes>
+        <Route path="/" element={<><Link to='todo'>todo(link)</Link><a href='todo'>todo(a)</a></>} />
+        <Route path="/todo" element={<Todo />} />
+        <Route path="*" element={<h1>Not found.</h1>} />
+      </Routes>
     </>
   );
 }
